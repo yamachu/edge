1ed167cb91794fa805aa3259bf1955318e602086
Yusuke Yamada <yamachu.dev@gmail.com>
2017-08-03 15:00:55 +0900

    Add support Electron 1.7.x

--- tools/buildall.bat
+++ tools/buildall.bat
@@ -1,2 +1,2 @@
 @echo off
-"%~dp0\build.bat" release 4.1.1 5.1.0 6.4.0 7.10.0 8.2.1 electron-1.3.13 electron-1.4.16 electron-1.6.11
+"%~dp0\build.bat" release 4.1.1 5.1.0 6.4.0 7.10.0 8.2.1 electron-1.3.13 electron-1.4.16 electron-1.6.11 electron-1.7.5
